Sonic-DE/sonic-globalacceld

Enhance modifier-only shortcuts
Trigger modifier-only shortcuts trigger on release, and support multi-key
modifier-only shortcuts, which trigger only when all the modifiers are
released within some interval.

BUG: 464805
BUG: 470256

src/globalshortcutsregistry.cpp
@@ -13,6 +13,7 @@
 #include "kglobalshortcutinfo_p.h"
 #include "kserviceactioncomponent.h"
 #include "logging_p.h"
+#include "sequencehelpers_p.h"
 #include <config-kglobalaccel.h>
 
 #include <KDesktopFile>
@@ -401,28 +402,27 @@ static void correctKeyEvent(int &keyQt)
         break;
     }
     keyQt = keySym | keyMod;
-
-    // When we are provided just a Shift key press, interpret it as "Shift" not as "Shift+Shift"
-    switch (keyQt) {
-    case (Qt::ShiftModifier | Qt::Key_Shift).toCombined():
-        keyQt = Qt::Key_Shift;
-        break;
-    case (Qt::ControlModifier | Qt::Key_Control).toCombined():
-        keyQt = Qt::Key_Control;
-        break;
-    case (Qt::AltModifier | Qt::Key_Alt).toCombined():
-        keyQt = Qt::Key_Alt;
-        break;
-    case (Qt::MetaModifier | Qt::Key_Meta).toCombined():
-        keyQt = Qt::Key_Meta;
-        break;
-    }
 }
 
 bool GlobalShortcutsRegistry::keyPressed(int keyQt)
 {
     correctKeyEvent(keyQt);
-    return handleNewKey(keyQt);
+    m_isReleasingModifierOnly = false;
+    int key = keyQt & ~Qt::KeyboardModifierMask;
+    Qt::KeyboardModifiers modifiers = static_cast<Qt::KeyboardModifiers>(keyQt & Qt::KeyboardModifierMask);
+    switch (key) {
+    case Qt::Key_Shift:
+    case Qt::Key_Control:
+    case Qt::Key_Alt:
+    case Qt::Key_Super_L:
+    case Qt::Key_Super_R:
+    case Qt::Key_Meta:
+        m_lastPressedModifiers = Utils::keyToModifier(key) | modifiers;
+        return false;
+    default:
+        m_lastPressedModifiers = Qt::NoModifier;
+        return handleNewKey(keyQt);
+    }
 }
 
 bool GlobalShortcutsRegistry::handleNewKey(int keyQt)
@@ -503,12 +503,33 @@ bool GlobalShortcutsRegistry::handleNewKey(int keyQt)
 
 bool GlobalShortcutsRegistry::keyReleased(int keyQt)
 {
-    Q_UNUSED(keyQt)
+    bool handled = false;
+    int key = keyQt & ~Qt::KeyboardModifierMask;
+    Qt::KeyboardModifiers modifiers = static_cast<Qt::KeyboardModifiers>(keyQt & Qt::KeyboardModifierMask);
+    switch (key) {
+    case Qt::Key_Super_L:
+    case Qt::Key_Super_R:
+    case Qt::Key_Meta:
+    case Qt::Key_Shift:
+    case Qt::Key_Control:
+    case Qt::Key_Alt:
+        modifiers &= ~Utils::keyToModifier(key);
+        constexpr auto releaseTimeout = std::chrono::milliseconds(200);
+        const auto currentTime = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now().time_since_epoch());
+        if (!m_isReleasingModifierOnly) {
+            m_isReleasingModifierOnly = true;
+            m_modifierFirstReleaseTime = currentTime;
+        }
+        if (!modifiers && (currentTime - m_modifierFirstReleaseTime) < releaseTimeout) {
+            handled = handleNewKey(m_lastPressedModifiers);
+            m_lastPressedModifiers = Qt::NoModifier;
+        }
+    }
     if (m_lastShortcut) {
         m_lastShortcut->context()->component()->emitGlobalShortcutReleased(*m_lastShortcut);
         m_lastShortcut = nullptr;
     }
-    return false;
+    return handled;
 }
 
 Component *GlobalShortcutsRegistry::createComponent(const QString &uniqueName, const QString &friendlyName)

src/sequencehelpers_p.cpp
@@ -59,6 +59,25 @@ bool contains(const QKeySequence &key, const QKeySequence &other)
     return ret;
 }
 
+Qt::KeyboardModifiers keyToModifier(int key)
+{
+    switch (key) {
+    case Qt::Key_Meta:
+    case Qt::Key_Super_L:
+    case Qt::Key_Super_R:
+        // Qt doesn't properly recognize Super_L/Super_R as MetaModifier
+        return Qt::MetaModifier;
+    case Qt::Key_Shift:
+        return Qt::ShiftModifier;
+    case Qt::Key_Control:
+        return Qt::ControlModifier;
+    case Qt::Key_Alt:
+        return Qt::AltModifier;
+    default:
+        return Qt::NoModifier;
+    }
+}
+
 bool matchSequences(const QKeySequence &key, const QList<QKeySequence> &keys)
 {
     // Since we're testing sequences, we need to check for all possible matches
@@ -82,6 +101,24 @@ bool matchSequences(const QKeySequence &key, const QList<QKeySequence> &keys)
     return false;
 }
 
+static int toModifierOnly(int keyQt)
+{
+    int key = keyQt & ~Qt::KeyboardModifierMask;
+    int mod = keyQt & Qt::KeyboardModifierMask;
+    switch (key) {
+    case Qt::Key_Shift:
+        return mod | Qt::ShiftModifier;
+    case Qt::Key_Control:
+        return mod | Qt::ControlModifier;
+    case Qt::Key_Alt:
+        return mod | Qt::AltModifier;
+    case Qt::Key_Meta:
+        return mod | Qt::MetaModifier;
+    default:
+        return keyQt;
+    }
+}
+
 QKeySequence mangleKey(const QKeySequence &key)
 {
     // Qt triggers both shortcuts that include Shift+Backtab and Shift+Tab
@@ -95,7 +132,7 @@ QKeySequence mangleKey(const QKeySequence &key)
         if ((keyMod & Qt::SHIFT) && (keySym == Qt::Key_Backtab || keySym == Qt::Key_Tab)) {
             k[i] = keyMod | Qt::Key_Tab;
         } else {
-            k[i] = key[i].toCombined();
+            k[i] = toModifierOnly(key[i].toCombined());
         }
     }
 

KDE/kglobalacceld

src/globalshortcutsregistry.cpp
@@ -13,6 +13,7 @@
 #include "kglobalshortcutinfo_p.h"
 #include "kserviceactioncomponent.h"
 #include "logging_p.h"
+#include "sequencehelpers_p.h"
 #include <config-kglobalaccel.h>
 
 #include <KDesktopFile>
@@ -401,28 +402,28 @@ static void correctKeyEvent(int &keyQt)
         break;
     }
     keyQt = keySym | keyMod;
-
-    // When we are provided just a Shift key press, interpret it as "Shift" not as "Shift+Shift"
-    switch (keyQt) {
-    case (Qt::ShiftModifier | Qt::Key_Shift).toCombined():
-        keyQt = Qt::Key_Shift;
-        break;
-    case (Qt::ControlModifier | Qt::Key_Control).toCombined():
-        keyQt = Qt::Key_Control;
-        break;
-    case (Qt::AltModifier | Qt::Key_Alt).toCombined():
-        keyQt = Qt::Key_Alt;
-        break;
-    case (Qt::MetaModifier | Qt::Key_Meta).toCombined():
-        keyQt = Qt::Key_Meta;
-        break;
-    }
 }
 
 bool GlobalShortcutsRegistry::keyPressed(int keyQt)
 {
     correctKeyEvent(keyQt);
-    return processKey(keyQt);
+    const int key = keyQt & ~Qt::KeyboardModifierMask;
+    const Qt::KeyboardModifiers modifiers = static_cast<Qt::KeyboardModifiers>(keyQt & Qt::KeyboardModifierMask);
+    switch (key) {
+    case Qt::Key_Shift:
+    case Qt::Key_Control:
+    case Qt::Key_Alt:
+    case Qt::Key_Super_L:
+    case Qt::Key_Super_R:
+    case Qt::Key_Meta:
+        m_state = PressingModifierOnly;
+        m_currentModifiers = Utils::keyToModifier(key) | modifiers;
+        return false;
+    default:
+        m_state = Normal;
+        m_currentModifiers = modifiers;
+        return processKey(keyQt);
+    }
 }
 
 bool GlobalShortcutsRegistry::processKey(int keyQt)
@@ -503,12 +504,42 @@ bool GlobalShortcutsRegistry::processKey(int keyQt)
 
 bool GlobalShortcutsRegistry::keyReleased(int keyQt)
 {
-    Q_UNUSED(keyQt)
+    correctKeyEvent(keyQt);
+    bool handled = false;
+    const int key = keyQt & ~Qt::KeyboardModifierMask;
+    const Qt::KeyboardModifiers modifiers = static_cast<Qt::KeyboardModifiers>(keyQt & Qt::KeyboardModifierMask);
+    switch (key) {
+    case Qt::Key_Super_L:
+    case Qt::Key_Super_R:
+    case Qt::Key_Meta:
+    case Qt::Key_Shift:
+    case Qt::Key_Control:
+    case Qt::Key_Alt: {
+        constexpr auto releaseTimeout = std::chrono::milliseconds(200);
+        const auto currentTime = std::chrono::steady_clock::now().time_since_epoch();
+        if (m_state == PressingModifierOnly) {
+            m_modifierOnlyModifiers = m_currentModifiers;
+            m_state = ReleasingModifierOnly;
+            m_modifierFirstReleaseTime = currentTime;
+        }
+        m_currentModifiers = modifiers & ~Utils::keyToModifier(key);
+        if (m_state == ReleasingModifierOnly && !m_currentModifiers) {
+            m_state = Normal;
+            if (currentTime - m_modifierFirstReleaseTime < releaseTimeout) {
+                handled = processKey(m_modifierOnlyModifiers);
+            }
+        }
+        break;
+    }
+    default:
+        m_state = Normal;
+        break;
+    }
     if (m_lastShortcut) {
         m_lastShortcut->context()->component()->emitGlobalShortcutReleased(*m_lastShortcut);
         m_lastShortcut = nullptr;
     }
-    return false;
+    return handled;
 }
 
 Component *GlobalShortcutsRegistry::createComponent(const QString &uniqueName, const QString &friendlyName)

src/sequencehelpers_p.cpp
@@ -59,6 +59,25 @@ bool contains(const QKeySequence &key, const QKeySequence &other)
     return ret;
 }
 
+Qt::KeyboardModifiers keyToModifier(int key)
+{
+    switch (key) {
+    case Qt::Key_Meta:
+    case Qt::Key_Super_L:
+    case Qt::Key_Super_R:
+        // Qt doesn't properly recognize Super_L/Super_R as MetaModifier
+        return Qt::MetaModifier;
+    case Qt::Key_Shift:
+        return Qt::ShiftModifier;
+    case Qt::Key_Control:
+        return Qt::ControlModifier;
+    case Qt::Key_Alt:
+        return Qt::AltModifier;
+    default:
+        return Qt::NoModifier;
+    }
+}
+
 bool matchSequences(const QKeySequence &key, const QList<QKeySequence> &keys)
 {
     // Since we're testing sequences, we need to check for all possible matches
@@ -82,6 +101,24 @@ bool matchSequences(const QKeySequence &key, const QList<QKeySequence> &keys)
     return false;
 }
 
+static int normalizeKey(int keyQt)
+{
+    int key = keyQt & ~Qt::KeyboardModifierMask;
+    int mod = keyQt & Qt::KeyboardModifierMask;
+    switch (key) {
+    case Qt::Key_Shift:
+        return mod | Qt::ShiftModifier;
+    case Qt::Key_Control:
+        return mod | Qt::ControlModifier;
+    case Qt::Key_Alt:
+        return mod | Qt::AltModifier;
+    case Qt::Key_Meta:
+        return mod | Qt::MetaModifier;
+    default:
+        return keyQt;
+    }
+}
+
 QKeySequence mangleKey(const QKeySequence &key)
 {
     // Qt triggers both shortcuts that include Shift+Backtab and Shift+Tab
@@ -95,7 +132,7 @@ QKeySequence mangleKey(const QKeySequence &key)
         if ((keyMod & Qt::SHIFT) && (keySym == Qt::Key_Backtab || keySym == Qt::Key_Tab)) {
             k[i] = keyMod | Qt::Key_Tab;
         } else {
-            k[i] = key[i].toCombined();
+            k[i] = normalizeKey(key[i].toCombined());
         }
     }
 