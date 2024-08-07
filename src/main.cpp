d69b21e333320b822930fb226a635b323a51048d
Nicolas Fella <nicolas.fella@gmx.de>
2024-08-07 16:34:05 +0200

    Set up crash handling
    (cherry picked from commit 5ddc3ccb69a5c14eae12b19a40f460b9129d77a3)

--- src/main.cpp
+++ src/main.cpp
@@ -48,6 +48,8 @@ int main(int argc, char **argv)
 
     KAboutData::setApplicationData(aboutdata);
 
+    KCrash::initialize();
+
     {
         QCommandLineParser parser;
         aboutdata.setupCommandLine(&parser);
